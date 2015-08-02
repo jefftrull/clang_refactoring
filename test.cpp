47dd53d773bfef1d97a775d1462c380e4a6e53cb
Jeff Trull <edaskel@att.net>
2015-08-02 15:55:53 -0700

    Add more interesting cases to test input

--- test.cpp
+++ test.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
 #include <string>
 
+void get_voltage(float & v) {
+    v = 3.3;
+}
+
 int main() {
     std::string str("blah blah blah");   // to be used const
-    double      d;                       // to be used ref
+    double      d;                       // to be used in assignment
+    float       f;                       // to be used as reference parameter
     int         i;                       // not used
     
-
     auto expression_capture_0 = [&]() -> void {
         std::cout << str << "\n";        // const usage
         d = 1.1;                         // non-const usage
+        get_voltage(f);                  // non-const usage
+
+        // a lambda that is NOT of interest to us
+        double c;
+        auto fn = [&]() -> double {
+            return c;        // a capture, but not one we introduced
+            float  d;        // re-use a name we already have
+            get_voltage(d);  // should NOT be flagged
+        };
+        std::cout << fn() << "\n";
+
     };
     expression_capture_0();
 
